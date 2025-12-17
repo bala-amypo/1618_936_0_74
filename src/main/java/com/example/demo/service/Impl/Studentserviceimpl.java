8e03e1f7e03035e6a5406283af6cffd3f494916e
bala-amypo <bala@amypo.in>
2025-12-17 09:10:52 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/Studentserviceimpl.java
+++ src/main/java/com/example/demo/service/Impl/Studentserviceimpl.java
@@ -2,9 +2,15 @@ package com.example.demo.service.Impl;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Studententity;
 import com.example.demo.service.Studentservice;
 
 @Service
-public class Studentserviceimpl implements Studentservice{           //splited as 2 layers here
+public class Studentserviceimpl implements Studentservice{ 
+ @Autowired Studentrepo student;    
+       //save()
+       //findAll()
+       //findByid()
+       //deleteBy       
         public Studententity postData(Studententity stu);
 }
