c331e767cc35ffb7b1342138fdbf3a8bb0756a1d
bala-amypo <bala@amypo.in>
2025-12-19 07:59:04 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/Timestampimpl.java
+++ src/main/java/com/example/demo/service/Impl/Timestampimpl.java
@@ -1,2 +1,38 @@
 package com.example.demo.service;
 
+public class Timestampimpl{
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.repository.Validationrepo;
+import com.example.demo.entity.Validationentity;
+import com.example.demo.service.Validationservice;
+import com.example.demo.exception.Validationexception;
+
+
+@Service
+public class Timestampserviceimpl implements Timestampservice{ //splited as 2 layers here
+ @Autowired Validationrepo student;    
+       //save()-insert,update
+       //findAll()
+       //findByid()
+       //deleteById()
+       //existsById()       
+       @Override
+        public Validationentity postData(Validationentity stu){
+        return student.save(stu);
+
+}
+
+      @Override
+        public Validationentity getData(long id){
+        return student.findById(id).orElseThrow(()->new Validationexception("Invalid Id" + id));
+
+
+
+}
+
+
+
+}
