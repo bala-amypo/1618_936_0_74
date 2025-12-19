36afbae26d0edc4dc2fe56d522e0942b92c23c66
bala-amypo <bala@amypo.in>
2025-12-19 10:23:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/Idcarddetail.java
+++ src/main/java/com/example/demo/entity/Idcarddetail.java
@@ -2,12 +2,23 @@ package com.example.demo.entity;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Entity;
-import jakarta.persistence.
-public class Idcarddetail{
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedType;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Timestamp{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
-    private String card_no;
-    private String email;
-    private Integer 
+    private Integer card_no;
+
 
     @OneToOne
     @JoinColumn(name = "student_id")
