9949112f11b41f2147ca84aa5bd1cab8d9a41aca
bala-amypo <bala@amypo.in>
2025-12-19 04:32:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/Validationcontroller.java
+++ src/main/java/com/example/demo/controller/Validationcontroller.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import jakarta.validation.Valid;
 
@@ -20,5 +23,7 @@ public class Validationcontroller{
         return ser.postData(stu);
      }  
      @GetMapping("/findV/{id}")
-     public Validationentity getDataval(@PathVariable long id) 
+     public Validationentity getDataval(@PathVariable long id){
+      return ser.getData(id);
+     } 
      }
